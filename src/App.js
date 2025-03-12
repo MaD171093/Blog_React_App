1c42c568fcceebf19881dd06ad8ec8e8c89b59e3
Manoj <md171093@gmail.com>
2025-03-12 14:31:52 -0400

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   const title = 'welcome to the new blog';
   const likes = 50;
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create/>
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
     </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The MD Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/" >New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" >New Blog</Link>
             </div>
         </nav>
     );
